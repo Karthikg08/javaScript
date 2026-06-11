9a59c8fac519ed9a0f81db74028839ef870003ac
Karthikg08 <kart03309@gmail.com>
2026-06-11 21:49:15 +0530

    reverse string

--- practice.js
+++ practice.js
@@ -57,4 +57,48 @@ console.log([2,-5,6,-5,8,0].map((value) => {
 // compact code of above method 
 
 console.log([2,-5,6,-5,8,0].map((value) =>  value +10)); //if the code is one line just remove the curly 
-                                                          //braces type into oneline just like that
+
+
+
+let users = [
+  { name: "Karthik", active: true },
+  { name: "Ram", active: false }
+];
+
+let activeUsers = users.filter(u => u.active);
+
+console.log(activeUsers);
+
+
+console.log('reduce method');
+
+let nums = [1, 2, 3, 4, 5]
+
+let result = nums.reduce((a, b) => a + b,0) // 0 is the initial value 
+//  without zero it cause error
+//  because it will take the first element of the array 
+// as the initial value of a and start adding from the second element 
+// but if the array is empty it will cause error 
+// because there is no initial value for a so we need to provide an initial value for a to avoid this error
+console.log(result) 
+
+
+let cart = [
+  { price: 100 },
+  { price: 200 },
+  { price: 300 },
+  { price: 300 }
+];
+
+let total = cart.reduce((sum, item) => sum + item.price, 0);
+console.log(total)
+
+
+let index = 7;
+
+ // ========== =  ==  === TYPING ANIMATION =========
+ // 
+ 
+ const str = "Hello World!";
+ const reversed = str.split('').reverse().join('')
+ console.log(reversed)
